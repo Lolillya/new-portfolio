30a284be8097be69ed7f67287a9611be24fbf036
Lolillya <someonesusernam@gmail.com>
2025-07-22 18:18:01 +0800

    removed header bg

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ import Logo from "@/assets/logo.png";
 export const Header = () => {
   return (
     <>
-      <header className="bg-bg-surface w-full backdrop-blur-md shadow-lg z-10">
+      <header className="w-full z-10">
         <div className="div-wrapper flex items-center">
           <img src={Logo} width={50} />
         </div>
