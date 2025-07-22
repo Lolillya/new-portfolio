5486079bdd04daacd76f1bb84580a612d0e40318
Lolillya <someonesusernam@gmail.com>
2025-07-22 15:27:09 +0800

    updated projects logo and const data

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,21 +1,42 @@
 import projectsData from "../constants/const.json";
 import { Button } from "./ui/button";
+import chedxLogo from '../assets/chedx-logo.svg';
+import infectino from '../assets/infectino.png';
+import prince from '../assets/prince.svg';
+import closeout from '../assets/closeout-connection.svg';
+import popstop from '../assets/popstop-logo.svg';
 
 
 export const Projects = () => {
   const projects = projectsData.projects;
+  const imageMap: Record<string, string> = {
+    'chedx-logo.svg': chedxLogo,
+    'infectino.png': infectino,
+    'prince.svg': prince,
+    'closeout-connection.svg': closeout,
+    'popstop-logo.svg': popstop,
+  };
   return (
     <>
       <section>
         <div className="w-full flex justify-center border-b border-border py-5">
           <span className="text-6xl">Projects</span>
         </div>
-        <div className="py-5 flex flex-col gap-8">
+        <div className="py-10 flex flex-col gap-8">
           {projects.map((project) => (
-            <div className="flex gap-4 justify-between" key={project.name}>
+            <div className="flex gap-4 justify-between py-5 border-b border-border rounded-md" key={project.name}>
               <div>
                 {/* <div className="bg-gray-400 w-[30rem] h-[30rem] flex items-center justify-center rounded-md"> */}
-                  <img src={project.img} alt={project.name} width={300} className="object-contain rounded-md" />
+                  <img 
+                    src={
+                      typeof project.img === 'string' && imageMap[project.img.split('/').pop() as string]
+                        ? imageMap[project.img.split('/').pop() as string]
+                        : project.img
+                    } 
+                    alt={project.name} 
+                    width={300} 
+                    className="object-contain rounded-md" 
+                  />
                   {/* Optionally add an image here if available */}
                 {/* </div> */}
               </div>
@@ -23,7 +44,6 @@ export const Projects = () => {
                 <span className="text-2xl">{project.name}</span>
                 <p className="text-justify">{project.desc}</p>
                 <div>
-                  <span>Tech Stack</span>
                   <ul className="flex gap-2 flex-wrap mt-1">
                     {project.stack.map((tech: string) => (
                       <li className="bg-surface shadow-md p-2 rounded-full text-xs" key={tech}>{tech}</li>
