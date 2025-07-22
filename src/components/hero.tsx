24035f4deaee48d0ac998b7b77f6c68d2cde4700
Lolillya <someonesusernam@gmail.com>
2025-07-22 18:05:43 +0800

    removed paragraph on hero section

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,21 +1,25 @@
+import logo from "@/assets/logo.png";
+
 
 export const Hero = () => {
   return (
     <>
-      <section id="section-0" className="mt-50">
+      <section id="section-0" className="mt-20">
         <div className="flex gap-3 ">
-          <div className="flex flex-col div-wrapper w-3/4 text-6xl gap-10">
+          <div className="flex flex-col div-wrapper w-3/4 text-6xl gap-10 justify-center">
             <span>Hello.</span>
             <span className="font-thin text-4xl">-----------I'm Jerald</span>
             <span className="font-light text-3xl">Fullstack Developer</span>
           </div>
 
           <div className="flex flex-col justify-center items-center w-full div-wrapper">
-            <p className="text-2xl font-thin text-justify">
-              I am a motivated and versatile individual, always eager to take on new challenges. With a passion for
-              learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth
-              mindset, I am ready to make a meaningful contribution and achieve great things.
-            </p>
+
+            <img src={logo} />
+            {/* <p className="text-2xl font-thin text-justify"> */}
+            {/*   I am a motivated and versatile individual, always eager to take on new challenges. With a passion for */}
+            {/*   learning, I am dedicated to delivering high-quality results. With a positive attitude and a growth */}
+            {/*   mindset, I am ready to make a meaningful contribution and achieve great things. */}
+            {/* </p> */}
 
           </div>
         </div>
