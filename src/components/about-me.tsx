dc0844ef6c11f64dea9cfafb7a21dd4344117f13
Lolillya <someonesusernam@gmail.com>
2025-11-07 04:24:21 +0800

    updated about me section

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,11 +1,8 @@
-
-
 export const AboutMe = () => {
   return (
     <>
       <section id="section-1">
         <div className="flex w-full items-center">
-
           <div className="w-full">
             <div className="border-l-2 border-border p-5 ml-2 my-2">
               <span className="text-2xl">Web Developer</span>
@@ -22,11 +19,16 @@ export const AboutMe = () => {
             <span className="text-6xl border-b border-border">About Me</span>
 
             <div>
-              <p className="text-2xl text-justify">I'm a passionate web developer with over 5 years of coding experience and a deep love for software engineering. I enjoy building clean, functional, and user-friendly web applications, and I'm constantly exploring new technologies to expand my skill set. Currently, I'm also diving into the world of blockchain development, driven by a strong interest in decentralized technologies and their potential to reshape the digital landscape. Whether it's crafting sleek front-end interfaces or exploring smart contracts, I'm always excited to learn, build, and push the limits of what software can do.</p>
+              <p className="text-2xl text-justify">
+                Passionate software engineer dedicated to building clean,
+                functional, and intuitive applications. Skilled at solving
+                complex problems, quickly learning new technologies, and
+                delivering software that drives real-world impact.
+              </p>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
-}
+  );
+};
